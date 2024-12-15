9103c4a9a5c1e93676a9be78260dd5512100c48b
kryphos <kryphos@proton.me>
2024-12-15 16:06:04 +0100

    ..

--- configs/nvimnew/lua/plugins/ui.lua
+++ configs/nvimnew/lua/plugins/ui.lua
@@ -65,6 +65,49 @@ return {
     {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
+        dependencies = {
+            { "HiPhish/rainbow-delimiters.nvim" },
+        },
+        config = function()
+            local highlight = {
+                "RainbowRed",
+                "RainbowYellow",
+                "RainbowBlue",
+                "RainbowOrange",
+                "RainbowGreen",
+                "RainbowViolet",
+                "RainbowCyan",
+            }
+            local hooks = require "ibl.hooks"
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+                vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+                vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+                vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+                vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+                vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+                vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            end)
+
+            vim.g.rainbow_delimiters = { highlight = highlight }
+            require("ibl").setup { scope = { highlight = highlight } }
+
+            hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+        end
+    },
+
+    {
+        "NvChad/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({
+                user_default_options = {
+                    mode = "virtualtext",
+                    virtualtext_inline = true,
+                    tailwind = true,
+                    always_update = true,
+                },
+            })
+        end
     },
 
     {
@@ -118,7 +161,7 @@ return {
                         },
                     },
                 },
-                open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+                open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
                 window = {
                     position = "left",
                     width = 30,
